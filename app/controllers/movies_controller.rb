10143488d264a2ee96417cfdfe20426ec00611ff
saasbook <saasbook@saasbook.(none)>
2012-10-31 08:21:01 -0700

    hw4 part 1

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -16,11 +16,11 @@ class MoviesController < ApplicationController
     end
     @all_ratings = Movie.all_ratings
     @selected_ratings = params[:ratings] || session[:ratings] || {}
-    
+
     if @selected_ratings == {}
       @selected_ratings = Hash[@all_ratings.map {|rating| [rating, rating]}]
     end
-    
+
     if params[:sort] != session[:sort]
       session[:sort] = sort
       flash.keep
@@ -64,4 +64,14 @@ class MoviesController < ApplicationController
     redirect_to movies_path
   end
 
+  def same_director
+    movie = Movie.find params[:id]
+    @movies = movie.find_with_same_director
+    if @movies == nil || @movies.empty?
+      flash[:notice] = "Movie '#{movie.title}' has no director info"
+      redirect_to movies_path
+    else
+      render 'same_director'
+    end
+  end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,12 @@ class Movie < ActiveRecord::Base
   def self.all_ratings
     %w(G PG PG-13 NC-17 R)
   end
+
+  def find_with_same_director
+    if (self.director != nil && self.director != '')
+      return Movie.find_all_by_director(self.director)
+    else
+      return []
+    end
+  end
 end
